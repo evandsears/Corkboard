0306cea85b7b72e30f70da6c8b0f3b8d73a65359
evandsears <evandsears@gmail.com>
2026-05-25 10:31:18 -0500

    chore: ignore Capacitor generated assets
    Add Capacitor-generated web assets and configuration to .gitignore to prevent committing build artifacts to version control.

--- android/app/src/main/assets/public/assets/index-BiejwQIw.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-IOm0VW26.js","assets/index-CCPSk4XU.js","assets/index-vvyhWA5w.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-CCPSk4XU.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-IOm0VW26.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
